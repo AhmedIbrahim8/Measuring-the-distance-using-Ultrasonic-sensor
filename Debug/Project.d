ec2a283a21ae520303ca84a047b9e3dcaa072db6
Ahmed-Ibrahim <eng.ahmedibrahim21@gmail.com>
2021-11-03 00:05:00 +0200

    Debug

---
+++ Debug/Project.d
@@ -0,0 +1,8 @@
+Project.o Project.o: ../Project.c ../lcd.h ../std_types.h \
+  ../ultrasonic_sensor.h
+
+../lcd.h:
+
+../std_types.h:
+
+../ultrasonic_sensor.h:
---
+++ Debug/ultrasonic_sensor.d
@@ -0,0 +1,13 @@
+ultrasonic_sensor.o ultrasonic_sensor.o: ../ultrasonic_sensor.c \
+  ../ultrasonic_sensor.h ../std_types.h ../common_macros.h ../icu.h \
+  ../gpio.h
+
+../ultrasonic_sensor.h:
+
+../std_types.h:
+
+../common_macros.h:
+
+../icu.h:
+
+../gpio.h:
